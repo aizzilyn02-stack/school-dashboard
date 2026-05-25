67aeca3fbf7a0f9aa5f83f021d1bc9e1acd7cc31
Aizzilyn Dela Cruz <aizzilyn02@gmail.com>
2026-05-25 20:09:27 +0800

    vercel fix v5

--- api/lambda.php
+++ api/lambda.php
@@ -13,6 +13,32 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+if (!empty(getenv('VERCEL'))) {
+    $logChannel = getenv('LOG_CHANNEL');
+    if ($logChannel === false || $logChannel === '') {
+        putenv('LOG_CHANNEL=errorlog');
+        $_ENV['LOG_CHANNEL'] = 'errorlog';
+        $_SERVER['LOG_CHANNEL'] = 'errorlog';
+    }
+
+    $requestedDatabase = getenv('DB_DATABASE');
+    if ($requestedDatabase === false || $requestedDatabase === '' || str_contains($requestedDatabase, 'database/database.sqlite')) {
+        $tmpDatabase = '/tmp/database.sqlite';
+        if (!file_exists($tmpDatabase)) {
+            $sourceDatabase = __DIR__.'/../database/database.sqlite';
+            if (file_exists($sourceDatabase)) {
+                copy($sourceDatabase, $tmpDatabase);
+            } else {
+                touch($tmpDatabase);
+            }
+        }
+
+        putenv("DB_DATABASE=$tmpDatabase");
+        $_ENV['DB_DATABASE'] = $tmpDatabase;
+        $_SERVER['DB_DATABASE'] = $tmpDatabase;
+    }
+}
+
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
--- config/logging.php
+++ config/logging.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('LOG_CHANNEL', 'stack'),
+    'default' => env('LOG_CHANNEL', !empty(getenv('VERCEL')) ? 'errorlog' : 'stack'),
 
     /*
     |--------------------------------------------------------------------------
